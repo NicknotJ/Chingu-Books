03bf94a1e3058da4bea1b6fb493681eab1ca908a
nicknotj <nicknotjustin@gmail.com>
2019-09-17 09:29:58 -0500

    removed console logs from bookFetch()

--- book-browser/src/BookFetch.js
+++ book-browser/src/BookFetch.js
@@ -1,11 +1,10 @@
 async function bookFetch(searchTerms, startIndex = 0){
-  console.log(startIndex);
+  
   let returnValue = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerms}&startIndex=${startIndex}`)
     .then((response) => {
       return response.json();
     })
     .then((myJson) => {
-      console.log(myJson);
       return myJson
     })
     .catch((err) => {
